a633cef7c523fb90741ce000bf259e6b443c6728
Esteban Quintero <albeesteban19@gmail.com>
2023-02-09 15:18:53 -0500

    actualizacion 0.1

--- src/com/doo/error403/app/PaginaWeb.java
+++ src/com/doo/error403/app/PaginaWeb.java
@@ -1,2 +1,8 @@
-package com.doo.error403.app;public class PaginaWeb {
+package com.doo.error403.app;
+
+public class PaginaWeb {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
 }
+//pagina web que se encargue de ser un mercado de motocicletas
